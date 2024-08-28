0c0b0c6ac3c3a88ef375d42d5a487de118db4e43
Goliardo17 <139957986+Goliardo17@users.noreply.github.com>
2024-08-28 21:53:42 +0300

    Update service categories
    переписал сервис категорий пользователя после обновления класса БД

--- server/services/user/categories/index.js
+++ server/services/user/categories/index.js
@@ -2,33 +2,22 @@ const { model } = require("../../../models");
 
 const db = model.user.categories;
 
-const get = ({ place, value }) => {
-  const data = db.data.filter((item) => item[place] === value);
-
-  return data.map((item) => item.category)
+const get = (filter, form = db.places) => {
+  return db.select(filter, form);
 };
 
-const create = ({ userId, categories }) => {
-  if (!userId || !categories.length) {
-    throw new Error({ message: "Отсутствуют необходимые поля" });
-  }
-
-  for (let category of categories) {
-    const newData = [...db.data];
-
-    const lastId = db.getLastId();
-
-    const id = lastId ? lastId + 1 : 1;
-
-    newData.push({ id, userId, category });
-
-    db.setData(newData);
-  }
+const create = (newData) => {
+  return db.insert(newData);
 };
 
-const categoriesData = {
+const delete = (filter) => {
+  return db.delete(filter);
+};
+
+const categoriesServices = {
   get,
   create,
+  delete,
 };
 
-module.exports = categoriesData;
+module.exports = categoriesServices;
