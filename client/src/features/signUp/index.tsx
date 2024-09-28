de8da550eb0bebb191566393b757cc9e98902dc5
Goliardo17 <goliardo17@yandex.ru>
2024-09-28 22:07:37 +0300

    TASK-10: add form for signUp account

--- client/src/features/signUp/index.tsx
+++ client/src/features/signUp/index.tsx
@@ -1,14 +1,40 @@
 import { SignUpProps } from "./types";
 
-export const SignUp: React.FC<SignUpProps> = ({ change }) => (
-    <form>
+const URL = "http://localhost:3001/user/create"
+
+export const SignUp: React.FC<SignUpProps> = ({ change }) => {
+  const submit = async (e: any) => {
+    e.preventDefault()
+    const HTMLform = e.target
+    const form = {
+      name: HTMLform.name.value,
+      email: HTMLform.email.value,
+      password: HTMLform.password.value,
+      birthday: HTMLform.birthday.value,
+      categories: HTMLform.categories.value,
+    }
+
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form)
+    })
+    const json = await response.json()
+
+    console.log(json)
+  }
+
+  return (
+    <form onSubmit={submit}>
       <p>Зарегистрироваться</p> <br />
-      <input placeholder="имя" /> <br />
-      <input placeholder="почта" /> <br />
-      <input placeholder="пароль" /> <br />
-      <input placeholder="дата рождения" /> <br />
-      <input placeholder="категории" /> <br />
+      <input name="name" placeholder="имя" /> <br />
+      <input name="email" placeholder="почта" /> <br />
+      <input name="password" placeholder="пароль" /> <br />
+      <input name="birthday" placeholder="дата рождения" /> <br />
+      <input name="categories" placeholder="категории" /> <br />
       <button>Зарегистрироваться</button> <br />
       <button onClick={change}>Уже есть акаунт</button> <br />
     </form>
-  )
+  )}
