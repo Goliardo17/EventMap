2d417d222200f328eb4b47dc79ffbb07da1afcca
Goliardo17 <goliardo17@yandex.ru>
2024-09-28 21:44:03 +0300

    TASK-9: add form for sigIn account

--- client/src/features/signIn/index.tsx
+++ client/src/features/signIn/index.tsx
@@ -1,13 +1,34 @@
 import { SignInProps } from "./types";
 
-const URL: string = 'http://localhost:3002/user'
+const URL: string = 'http://localhost:3001/user/enter'
 
-export const SignIn: React.FC<SignInProps> = ({ change }) => (
-  <form method="get">
+export const SignIn: React.FC<SignInProps> = ({ change }) => {
+  const submit = async (e: any): Promise<void> => {
+    e.preventDefault()
+    const HTMLform = e.target
+    const form = {
+      email: HTMLform.email.value,
+      password: HTMLform.password.value,
+    }
+
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form)
+    })
+    const json = await response.json()
+
+    console.log(json);
+  }
+
+  return (
+  <form onSubmit={submit}>
     <p>Войти</p> <br />
-    <input placeholder="почта" /> <br />
-    <input placeholder="пароль" /> <br />
+    <input name="email" placeholder="почта" /> <br />
+    <input name="password" placeholder="пароль" /> <br />
     <button onClick={change}>Зарегистрироваться</button> <br />
-    <button type="submit" formAction={URL}>Вход</button> <br />
+    <button type="submit">Вход</button> <br />
   </form>
-)
+)}
